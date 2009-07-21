d1f36b233268a94096cb95e0f1394fcd9d4ebd4b
gilihamu <gilihamu@79ffb6b6-719a-11de-8b73-c1edde54a3c7>
2009-07-21 23:02:58 +0000

    DAO SOLICITACAO

---
+++ sams/src/java/dao/SolicitacaoDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+import java.util.List;
+import model.Solicitacao;
+import org.hibernate.Session;
+
+
+/**
+ *
+ * @author Mattheus Pirovani
+ */
+public class SolicitacaoDao extends GenericDao{
+
+    private static final long serialVersionUID = 1L;
+    private Session session;
+
+    public SolicitacaoDao(Session session) {
+        this.session = session;
+    }
+
+    public SolicitacaoDao() {
+        this.session = getSession();
+    }
+
+    public void limpar(){
+    }
+
+    public void salvar(Solicitacao solicitacao){
+      savePojo(solicitacao);
+    }
+
+    public void alterar(Solicitacao solicitacao) {
+        saveorUpdatePojo(solicitacao);
+    }
+
+    public void excluir(Solicitacao solicitacao) {
+        removePojo(solicitacao);
+    }
+
+
+}
